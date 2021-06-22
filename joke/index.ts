65cf6c32afccb6273dd4224f46e2b400b3d5a8d1
Emil Styrke <emil.styrke@axis.com>
2021-06-22 13:35:37 +0200

    feat: refactor and new jokes

--- joke/index.ts
+++ joke/index.ts
@@ -6,9 +6,11 @@ type JokeResponse = {
 
 const jokes = [
   "Hur ser man att en bil kommer från Tyskland? - Det Germany'nte",
+  "Hur ser man att en bil kommer från Frankrike? - På vindruvetorkarna",
   "Det var en bonde som inte hade någon hink, istället hade han en katt som spann.",
   "Varför kan inte blinda programmera? - Dom kan ju inte C.",
   "Hur känner man igen en äkta Nintendo-nörd? - Han dricker NESCafé med 8 bitar socker.",
+  "Hur låter ett spökfår? - Varken bu eller bä",
 ];
 
 const randomElement = <T>(array: T[]) =>
@@ -17,12 +19,12 @@ const randomElement = <T>(array: T[]) =>
 const httpTrigger: AzureFunction = async function (
   context: Context,
   req: HttpRequest
-): Promise<void> {
+): Promise<any> {
   const response: JokeResponse = {
     text: randomElement(jokes),
   };
 
-  context.res = {
+  return {
     // status: 200, /* Defaults to 200 */
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(response),
